d7aeaa82e2e9cc3184e416125c74cbe61dfc418b
mattstam <codeguy0112358@gmail.com>
2025-05-21 14:07:01 -0700

    add tests

---
+++ contracts/src/mocks/MockVerifier.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {ISP1Verifier} from "../interfaces/ISP1Verifier.sol";
+
+contract MockVerifier is ISP1Verifier {
+    function verifyProof(
+        bytes32,
+        bytes calldata,
+        bytes calldata
+    ) external view {
+        // No-op
+    }
+}
