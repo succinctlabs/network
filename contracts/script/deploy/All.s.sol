289f7f2c4079abcb5ec5f79b5568a75334f791ef
mattstam <codeguy0112358@gmail.com>
2025-06-06 22:04:01 -0700

    feat(contracts): deploy

--- contracts/script/deploy/All.s.sol
+++ contracts/script/deploy/All.s.sol
@@ -7,16 +7,14 @@ import {SuccinctVApp} from "../../src/SuccinctVApp.sol";
 import {IntermediateSuccinct} from "../../src/tokens/IntermediateSuccinct.sol";
 import {SuccinctGovernor} from "../../src/SuccinctGovernor.sol";
 import {Succinct} from "../../src/tokens/Succinct.sol";
-import {FixtureLoader, SP1ProofFixtureJson, Fixture} from "../../test/utils/FixtureLoader.sol";
+import {FixtureLoader} from "../../test/utils/FixtureLoader.sol";
 import {ERC1967Proxy} from
     "../../lib/openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {SP1VerifierGateway} from "../../lib/sp1-contracts/contracts/src/SP1VerifierGateway.sol";
+import {SP1Verifier} from "../../lib/sp1-contracts/contracts/src/v4.0.0-rc.3/SP1VerifierGroth16.sol";
 
 // Deploy all contracts.
 contract AllScript is BaseScript, FixtureLoader {
-    // Get from the corresponding chain deployment here:
-    // https://github.com/succinctlabs/sp1-contracts/tree/main/contracts/deployments
-    address internal SP1_VERIFIER_GATEWAY_GROTH16 = 0x397A5f7f3dBd538f23DE225B51f532c34448dA9B;
-
     function run() external broadcaster {
         // Read config
         bytes32 salt = readBytes32("CREATE2_SALT");
@@ -30,7 +28,7 @@ contract AllScript is BaseScript, FixtureLoader {
         address STAKING = address(new SuccinctStaking{salt: salt}(OWNER));
         address PROVE = address(new Succinct{salt: salt}(OWNER));
         address I_PROVE = address(new IntermediateSuccinct{salt: salt}(PROVE, STAKING));
-        address VAPP = _deployVAppAsProxy(salt, PROVE, I_PROVE, STAKING);
+        (address VERIFIER, address VAPP) = _deployVAppAsProxy(salt, OWNER, PROVE, I_PROVE, STAKING);
         address GOVERNOR = address(new SuccinctGovernor{salt: salt}(STAKING));
 
         // Initialize staking contract
@@ -40,6 +38,7 @@ contract AllScript is BaseScript, FixtureLoader {
 
         // Write addresses
         writeAddress("STAKING", STAKING);
+        writeAddress("VERIFIER", VERIFIER);
         writeAddress("VAPP", VAPP);
         writeAddress("PROVE", PROVE);
         writeAddress("I_PROVE", I_PROVE);
@@ -47,26 +46,46 @@ contract AllScript is BaseScript, FixtureLoader {
     }
 
     /// @dev This is a stack-too-deep workaround.
-    function _deployVAppAsProxy(bytes32 salt, address PROVE, address I_PROVE, address STAKING)
+    function _deployVAppAsProxy(bytes32 salt, address OWNER, address PROVE, address I_PROVE, address STAKING)
         internal
-        returns (address)
+        returns (address, address)
     {
         // Read config
-        address VERIFIER = SP1_VERIFIER_GATEWAY_GROTH16;
+        address VERIFIER = vm.envOr("VERIFIER", address(0));
+        bytes32 VKEY = bytes32(0x0027860ee5a98f9ad361de3622d6eaf4a9caf2c2354395332fc95254eddab0f6);
+        bytes32 GENESIS_STATE_ROOT =
+            bytes32(0x4b15a7d34ea0ec471d0d6ab9170cc2910f590819ee168e2a799e25244e327116);
+        uint64 GENESIS_TIMESTAMP = 0;
 
-        bytes32 VKEY = bytes32(0x004a38f8086f29b885cbb1c36a31ba540a7fdb97e43c10e52edb3e88373cc7a1);
-        bytes32 genesisStateRoot =
-            bytes32(0xbd80a17ca4b24913817da39707526bd5cd95b61a1e50c39e59481ccca363452c);
-        uint64 genesisTimestamp = 0;
+        if (VERIFIER == address(0)) {
+            // Deploy the SP1VerifierGatway
+            VERIFIER = address(new SP1VerifierGateway{salt: salt}(OWNER));
+            address groth16 = address(new SP1Verifier{salt: salt}());
+            SP1VerifierGateway(VERIFIER).addRoute(groth16);
+        }
 
         // Deploy contract
         address vappImpl = address(new SuccinctVApp{salt: salt}());
         address VAPP =
             address(SuccinctVApp(payable(address(new ERC1967Proxy{salt: salt}(vappImpl, "")))));
         SuccinctVApp(VAPP).initialize(
-            msg.sender, PROVE, I_PROVE, STAKING, VERIFIER, VKEY, genesisStateRoot, genesisTimestamp
+            msg.sender, PROVE, I_PROVE, STAKING, VERIFIER, VKEY, GENESIS_STATE_ROOT, GENESIS_TIMESTAMP
         );
 
-        return VAPP;
+        return (VERIFIER, VAPP);
     }
 }
+
+//     at bin/vapp/src/main.rs:79
+
+//   2025-06-07T01:20:49.943576Z  INFO spn_vapp::driver: stf vkey bytes32: "0x0071c8d938846d01d3b3ef216192c1848be9077c74ce0a32c95e7c703b8c442a"
+//     at bin/vapp/src/driver.rs:142
+
+//   2025-06-07T01:20:50.462312Z  INFO spn_vapp::driver: aggregation vkey bytes32: "0x0027860ee5a98f9ad361de3622d6eaf4a9caf2c2354395332fc95254eddab0f6"
+//     at bin/vapp/src/driver.rs:146
+
+//   2025-06-07T01:20:50.462474Z  INFO spn_vapp::driver: genesis state root: 0x4b15a7d34ea0ec471d0d6ab9170cc2910f590819ee168e2a799e25244e327116
+//     at bin/vapp/src/driver.rs:158
+
+//   2025-06-07T01:20:50.647385Z  INFO spn_vapp::driver: Initialized with genesis state 4b15a7d34ea0ec471d0d6ab9170cc2910f590819ee168e2a799e25244e327116 at cursor 1
+//     at bin/vapp/src/driver.rs:348
