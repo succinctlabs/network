86f13ccac800d3e301c3023cac0c2143c2b26c33
mattstam <codeguy0112358@gmail.com>
2025-06-20 16:25:38 -0700

    good

--- contracts/test/SuccinctStaking.fulfillment.t.sol
+++ contracts/test/SuccinctStaking.fulfillment.t.sol
@@ -44,7 +44,7 @@ contract SuccinctStakingFulfillmentTests is SuccinctStakingTest {
         BalanceSnapshot memory before = _takeSnapshot();
 
         // Calculate expected reward split including protocol fee
-        (uint256 expectedProtocolFee, uint256 expectedStakerReward, uint256 expectedOwnerReward) =
+        (uint256 _expectedProtocolFee, uint256 _expectedStakerReward, uint256 _expectedOwnerReward) =
             _calculateFullRewardSplit(rewardAmount);
 
         // Reward the prover
